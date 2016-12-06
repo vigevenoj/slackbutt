e6f0e2d4c8660e978c3a612613387aef13f2ce90
Jacob Vigeveno <vigevenoj@gmail.com>
2016-12-05 17:07:28 -0800

    the cat has spoken: optional longitude and latitude

--- opt/slackbutt/plugins/spacestation.py
+++ opt/slackbutt/plugins/spacestation.py
@@ -5,16 +5,36 @@ import requests
 from datetime import datetime, timedelta
 
 URL = 'http://api.open-notify.org/iss-pass.json?lat={0}&lon={1}'
+LONGITUDE = -122.680372
+LATITUDE = 45.522005
 
 
 class SpaceStation():
-    def __init__(self):
-        self._latitude = 45.522005
-        self._longitude = 45.522005
+    def __init__(self, longitude=-122.680372, latitude=45.522005):
+        if (latitude is None):
+            self._latitude = 45.522005
+        else:
+            self._latitude = latitude
+        if (longitude is None):
+            self._longitude = -122.680372
+        else:
+            self._longitude = longitude
 
-#    @property
-#    def location(self):
-#        return (self._longitude, self._latitude)
+    @property
+    def longitude(self):
+        return self._longitude
+
+    @property
+    def latitude(self):
+        return self._latitude
+
+    @longitude.setter
+    def longitude(self, longitude):
+        self._longitude = longitude
+
+    @latitude.setter
+    def latitude(self, latitude):
+        self._latitude = latitude
 
     def next_pass(self):
         reply = "nobody knows anything about a space station"
@@ -36,18 +56,22 @@ class SpaceStation():
             print "status code was {0}".format(data.status_code)
         return reply
 
-ISS_STRING = r'''iss'''
+ISS_STRING = r'''iss\s?((-?[\d]+\.[\d]+)[,]?\s(-?[\d]+\.[\d]+))?'''
 ISS = re.compile(ISS_STRING, re.IGNORECASE)
 
 
 @slackbot.bot.respond_to(ISS)
 def do_iss(message, *groups):
     print "someone asked about the international space station!"
-    iss = SpaceStation()
+    try:
+        longitude, latitude = groups[1], groups[2]
+    except IndexError:
+        longitude, latitude = LONGITUDE, LATITUDE  # constants for pdx
+    iss = SpaceStation(longitude, latitude)
     msg = iss.next_pass()
     message.reply(msg)
 
 
 if __name__ == '__main__':
-    iss = SpaceStation()
-    iss.next_pass()
+    iss = SpaceStation(LONGITUDE, LATITUDE)
+    print iss.next_pass()
